d2779128d11c74acee2bb180365630a19a7d4dba
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-27 16:02:02 +0800

    feat:add counter

---
+++ src/component/CounterPractice.jsx
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    function decrease() {
+        setCount(count - 1);
+    }
+
+    function increase(){
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <button onClick={increase}>
+                +
+            </button>
+            <span>{count}</span>
+            <button onClick={decrease}>
+                -
+            </button>
+        </div>
+    );
+}
